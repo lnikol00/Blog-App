70c00de1e2d9b2e5fa485b1094c5c29d81365161
Koce1 <nikolac007@gmail.com>
2023-03-21 23:14:06 +0100

    correction

--- src/pages/home/BlogList.tsx
+++ src/pages/home/BlogList.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from "./home.module.css"
 
 interface BlogProps {
     blogs: ToDoItems[],
+    search: string
 }
 
 interface ToDoItems {
@@ -15,10 +16,21 @@ interface ToDoItems {
 
 function BlogList(props: BlogProps) {
     const blogs = props.blogs
+    const search = props.search
     return (
         <div className={styles.blogContainer}>
-            <h2></h2>
-            {blogs.map((blog) => {
+            {blogs.filter((blog) => {
+                if (search === "") {
+                    return blog;
+                }
+                else if (blog.title.toLocaleLowerCase().includes(search.toLocaleLowerCase())) {
+                    return blog;
+                }
+                else if (blog.author.toLocaleLowerCase().includes(search.toLocaleLowerCase())) {
+                    return blog;
+                }
+
+            }).map((blog) => {
                 return (
                     <div className={styles.blogPreview} key={blog.id}>
                         <Link to={`blogs/${blog.id}`}>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,8 @@ import styles from "./home.module.css"
 import BlogList from './BlogList'
 import useFetch from '../../components/hooks/useFetch'
 import useAuth from "../../components/hooks/useAuth"
+import { useState } from "react"
+import * as BsIcons from "react-icons/bs"
 
 interface ToDoItems {
     id: number,
@@ -11,15 +13,22 @@ interface ToDoItems {
 }
 
 function Home() {
-    const [data, isPending, error] = useFetch<ToDoItems[]>('http://localhost:8000/blogs', []);
     const { auth } = useAuth();
+    const [data, isPending, error] = useFetch<ToDoItems[]>('http://localhost:8000/blogs', []);
+    const [search, setSearch] = useState<string>('')
     // To fetch data from database run "npx json-server --watch src/data/db.json --port 8000" in terminal
     return (
         <div className={styles.mainContainer}>
-            <h2>Welcome {auth?.user}</h2>
+            <div className={styles.heading}>
+                <h2>Welcome {auth?.user}</h2>
+                <label>
+                    <input onChange={e => setSearch(e.target.value)} />
+                    <BsIcons.BsSearch />
+                </label>
+            </div>
             <span>Your blogs are:</span>
             {isPending && <div className={styles.loading}>Loading...</div>}
-            {data && <BlogList blogs={data} />}
+            {data && <BlogList blogs={data} search={search} />}
             {error && <div className={styles.error}>{error}</div>}
         </div>
     )
